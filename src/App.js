e8e473d4a9e06c17e9beaee04ba9df9dcb393d40
JGiles <jgiles@infosis-arg.com>
2023-05-23 04:23:15 -0300

    version 0.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+export const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [cities, setCities] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(`http://localhost:8081/topCities/search?query=${searchTerm}`);
+      setCities(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacture
-        </a>
-      </header>
+    <div>
+      <h1>City Weather Explorer</h1>
+      <div>
+        <input type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <ul>
+        {cities.map(city => (
+          <li key={city.id}>
+            {city.localizedName} - {city.temperatureValue} {city.temperatureUnit} - {city.weatherText}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
